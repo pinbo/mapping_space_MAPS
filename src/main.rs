66c4d6a72cd052aa939e8bb7ac43fe7c79a9b350
Junli Zhang <zhjl86@gmail.com>
2021-01-18 16:01:22 -0800

    add minCov to argument

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use std::io::{self, BufRead, BufReader};
-// use std::env;
+use std::env;
 
 fn main() {
-    // let args: Vec<String> = env::args().collect();
-    // let ncol: u32 = args[1].parse().expect("Please type a number!"); // file number of columns
-    // println!("Input has {} columns", ncol);
+    let args: Vec<String> = env::args().collect();
+    let min_cov: u32 = args[1].parse().expect("Please give a number of minimum coverage"); // minimum coverage at a position
+    println!("minimum coverage is {}", min_cov);
 
     let input = io::stdin();
     let mut reader = BufReader::new(input.lock());
@@ -15,7 +15,7 @@ fn main() {
     println!("First line has {} columns and {} samples.", ncol, nsample);
     let max_missing = 4;
     let mut good_lines = 0;
-    let min_cov = 2;
+    // let min_cov = 2;
     // let min_cov_str = min_cov.to_string();
 
     for line in reader.lines() {
@@ -32,7 +32,7 @@ fn main() {
                     nmiss += 1;
                     if nmiss > max_missing {break}
                 } else {
-                    let nn: usize = word.parse().unwrap();
+                    let nn: u32 = word.parse().unwrap();
                     if nn >= min_cov {
                         ngood += 1;
                     } else {
@@ -44,7 +44,8 @@ fn main() {
                 continue;
             }
         }
-        if ngood >= nsample - min_cov  {
+        println!("nsample - max_missing is {}", nsample - max_missing);
+        if ngood >= nsample - max_missing {
             good_lines += 1;
         }
     }
