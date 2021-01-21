55d1db0f62540f7b12e231539afec4da638c137a
Junli Zhang <zhjl86@gmail.com>
2021-01-20 22:34:45 -0800

    add cmd line options

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,30 @@
 // v0.2.0: output A T G C counts
 
 use std::io::{self, BufRead, BufReader};
-use std::env;
+// use std::env;
 use std::collections::HashMap;
+use clap::App;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let min_cov: u32 = args[1].parse().expect("Please give a number of minimum coverage"); // minimum coverage at a position
+    // parse options
+    let matches = App::new("Calc_mapping_space")
+        .version("0.3.0")
+        .author("Junli Zhang <zhjl86@gmail.com>")
+        .about("Calculate mapping space and %GC from MAPS output parsed_Kronos_mpileup.txt.gz")
+        .arg("-m, --max_missing=[NUMBER] 'maxium number of mising libs'")
+        .arg("-c, --min_cov=[NUMBER]              'minimum coverage at positions'")
+        .get_matches();
+    // get the values
+    // Gets a value for config if supplied by user, or defaults to "default.conf"
+    // let config = matches.value_of("config").unwrap_or("default.conf");
+    let min_cov: u32 = matches.value_of("min_cov").unwrap_or("1").parse().expect("Please give a number of minimum coverage"); // minimum coverage at a position
     println!("minimum coverage is {}", min_cov);
+    let max_missing: usize = matches.value_of("max_missing").unwrap_or("4").parse().expect("Please give a number of maximum missing libs");
+    println!("Maximum missing libs allowed is {}", max_missing);
+
+    // let args: Vec<String> = env::args().collect();
+    // let min_cov: u32 = args[1].parse().expect("Please give a number of minimum coverage"); // minimum coverage at a position
+    // println!("minimum coverage is {}", min_cov);
 
     let input = io::stdin();
     let mut reader = BufReader::new(input.lock());
@@ -16,12 +33,10 @@ fn main() {
     let ncol = first_line.split("\t").count();
     let nsample = (ncol - 3) / 4;
     println!("First line has {} columns and {} samples.", ncol, nsample);
-    let max_missing = 4;
+    // let max_missing = 4;
     let min_lib_count = nsample - max_missing;
     let mut good_lines = 0;
     let mut map = HashMap::new(); // count A, T, G, C
-    // let min_cov = 2;
-    // let min_cov_str = min_cov.to_string();
 
     for line in reader.lines() {
         let mut nmiss = 0;
@@ -73,7 +88,6 @@ fn main() {
         if nts.contains(key) {total += value;}
         if gc.contains(key) {gc_count += value;}
     }
-    // let pct = gc / nts;
     println!("GC% is {:.2}%", (gc_count as f64 / total as f64) * 100.0);
 }
 
